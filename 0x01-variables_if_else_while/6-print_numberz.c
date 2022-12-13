19e950624f45bcbee9e241fc6a101227c5580565
RoyB26 <royhanb62@gmaiyhanb62@gmail.com>
2022-12-12 16:23:52 -0800

    task 7

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - print numbers from base 10 starting from 0
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int n;
+
+for (n = 0; n < 10; n++)
+{
+	putchar("%d", n);
+}
+	putchar("\n");
+	return (0);
+}
